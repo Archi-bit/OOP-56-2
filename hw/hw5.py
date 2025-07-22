eb779d5e8cb97884fb9c19f950d5093d072d2f4d
Archi-bit <argennnmmmtv@gmail.com>
2025-07-22 19:39:50 +0600

    hw: hw 5 added

---
+++ hw/hw5.py
@@ -0,0 +1,20 @@
+def check_admin(is_admin: bool):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            if is_admin:
+                return func(*args, **kwargs)
+            else:
+                print("Доступ запрещен. Разрешен только для админов!")
+        return wrapper
+    return decorator
+
+@check_admin(is_admin=True)
+def delete_user():
+    print("Пользователь уделен")
+
+@check_admin(is_admin=False)
+def delete_post():
+    print("Пост удален")
+
+delete_user()
+delete_post()
